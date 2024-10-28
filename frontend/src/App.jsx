cf56f7e4d4de955e727b7a1ba90fb2f3abd66871
Emily Ho <114359555+Emilyho11@users.noreply.github.com>
2024-10-28 16:06:39 -0400

    Adding footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Navbar from './components/Navbar'
+import './App.css';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Library from './pages/Library';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
-					<Navbar />
-					<div className="flex-1 bg-white relative">
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/myLibrary" element={<Library />} />
-						</Routes>
-					</div>
-				</div>
+        <Navbar />
+        <div className="flex-grow bg-white">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/myLibrary" element={<Library />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='items-center text-white bg-clio_color/90 p-4 flex flex-row-reverse'>
+      <p className='text-sm'>Emily Ho ©2024</p>
+      <p className='text-sm pr-4 mr-4 border-r-2'>Legal Sales Target App</p>
+    </div>
+  )
+}
+export default Footer
